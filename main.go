4d78a0d1ded9ee567a80978715c5df2eb80e2de4
nικήτας <mac@MacBook-Air-mac.local>
2025-12-03 19:03:07 +0300

    working go calc

--- main.go
+++ main.go
@@ -3,5 +3,31 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Привет мир!")
+
+	var a, b float64
+	var op string
+
+	fmt.Print("Enter first num: ")
+	fmt.Scan(&a)
+	fmt.Print("Enter second num: ")
+	fmt.Scan(&b)
+	fmt.Print("Input operation (+, -, *, /): ")
+	fmt.Scan(&op)
+
+	switch op {
+	case "+":
+		fmt.Println("Res:", a+b)
+	case "-":
+		fmt.Println("Res: ", a-b)
+	case "*":
+		fmt.Println("Res: ", a*b)
+	case "/":
+		if b == 0 {
+			fmt.Println("Error! devision by zero!")
+		} else {
+			fmt.Println("Res: ", a/b)
+		}
+	default:
+		fmt.Println("Unknown opreation. Try again.")
+	}
 }
