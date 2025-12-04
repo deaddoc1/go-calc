e3bc69cfe348a1b8c8939fe1495e48e1f98e772c
nικήτας <mac@MacBook-Air-mac.local>
2025-12-04 13:00:08 +0300

    more op mor calc

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func add(a, b float64) float64 {
 	return a + b
@@ -21,6 +24,29 @@ func div(a, b float64) (float64, error) {
 	return a / b, nil
 }
 
+func pow(a, b float64) float64 {
+	return math.Pow(a, b)
+}
+
+func sqrt(a float64) float64 {
+	return math.Sqrt(a)
+}
+
+func mod(a, b float64) (int, error) {
+	if b == 0 {
+		return 0, fmt.Errorf("division by zero")
+	}
+	return int(a) % int(b), nil
+}
+
+func square(a float64) float64 {
+	return a * a
+}
+
+func abs(a float64) float64 {
+	return math.Abs(a)
+}
+
 func main() {
 
 	for {
@@ -30,18 +56,21 @@ func main() {
 
 		fmt.Print("Enter first num: ")
 		fmt.Scan(&a)
+		fmt.Print("Input operation (+, -, *, /, ^, sqrt, %, square, abs): ")
+		fmt.Scan(&op)
 		fmt.Print("Enter second num: ")
 		fmt.Scan(&b)
-		fmt.Print("Input operation (+, -, *, /): ")
-		fmt.Scan(&op)
 
 		switch op {
 		case "+":
 			fmt.Println("Res:", add(a, b))
+
 		case "-":
 			fmt.Println("Res: ", sub(a, b))
+
 		case "*":
 			fmt.Println("Res: ", mul(a, b))
+
 		case "/":
 			result, err := div(a, b)
 			if err != nil {
@@ -49,6 +78,27 @@ func main() {
 			} else {
 				fmt.Println("Res: ", result)
 			}
+
+		case "^":
+			fmt.Println("Res: ", pow(a, b))
+
+		case "sqrt":
+			fmt.Println("Res: ", sqrt(a))
+
+		case "%":
+			result, err := mod(a, b)
+			if err != nil {
+				fmt.Println("Error: ", err)
+			} else {
+				fmt.Println("Res: ", result)
+			}
+
+		case "square":
+			fmt.Println("Res: ", square(a))
+
+		case "abs":
+			fmt.Println("Res: ", abs(a))
+
 		default:
 			fmt.Println("Unknown opreation. Try again.")
 		}
