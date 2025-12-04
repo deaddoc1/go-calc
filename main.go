7e266e3e2c3aa199f51f55d3cc554763d477f171
nικήτας <mac@MacBook-Air-mac.local>
2025-12-04 12:01:45 +0300

    for for calc

--- main.go
+++ main.go
@@ -4,30 +4,42 @@ import "fmt"
 
 func main() {
 
-	var a, b float64
-	var op string
+	for {
 
-	fmt.Print("Enter first num: ")
-	fmt.Scan(&a)
-	fmt.Print("Enter second num: ")
-	fmt.Scan(&b)
-	fmt.Print("Input operation (+, -, *, /): ")
-	fmt.Scan(&op)
+		var a, b float64
+		var op string
 
-	switch op {
-	case "+":
-		fmt.Println("Res:", a+b)
-	case "-":
-		fmt.Println("Res: ", a-b)
-	case "*":
-		fmt.Println("Res: ", a*b)
-	case "/":
-		if b == 0 {
-			fmt.Println("Error! devision by zero!")
-		} else {
-			fmt.Println("Res: ", a/b)
+		fmt.Print("Enter first num: ")
+		fmt.Scan(&a)
+		fmt.Print("Enter second num: ")
+		fmt.Scan(&b)
+		fmt.Print("Input operation (+, -, *, /): ")
+		fmt.Scan(&op)
+
+		switch op {
+		case "+":
+			fmt.Println("Res:", a+b)
+		case "-":
+			fmt.Println("Res: ", a-b)
+		case "*":
+			fmt.Println("Res: ", a*b)
+		case "/":
+			if b == 0 {
+				fmt.Println("Error! devision by zero!")
+			} else {
+				fmt.Println("Res: ", a/b)
+			}
+		default:
+			fmt.Println("Unknown opreation. Try again.")
 		}
-	default:
-		fmt.Println("Unknown opreation. Try again.")
+
+		var cont string
+		fmt.Print("Continue? (y/n): ")
+		fmt.Scan(&cont)
+
+		if cont != "y" && cont != "Y" {
+			fmt.Println("Goodbye!")
+			break
+		}
 	}
 }
